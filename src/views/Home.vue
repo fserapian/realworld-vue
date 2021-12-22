9c718370607c554e536ae84785613ce8d24b37f3
Fadi Serapian <fadi.serapian@gmail.com>
2021-12-22 15:13:58 -0500

    init and add register view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>this is home</h1>
+    <HelloWorld />
   </div>
 </template>
 
---
+++ src/views/Register.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="auth-page">
+    Register page
+  </div>
+</template>
+
+<script>
+</script>
