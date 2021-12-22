eb924142d85aee570c4e3fab0ebc6734c40946b9
Fadi Serapian <fadi.serapian@gmail.com>
2021-12-22 15:21:35 -0500

    add markup to register view

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,8 +1,41 @@
 <template>
   <div class="auth-page">
-    Register page
+    <div class="row">
+      <div class="col-md-6 offset-md-3 col-xs-12">
+        <h1 class="text-xs-center">Sign up</h1>
+        <p class="text-xs-center">
+          <router-link :to="{ name: 'login' }">Need an account?</router-link>
+        </p>
+        VALIDATION ERRORS
+        <form action="">
+          <fieldset class="form-group">
+            <input
+              type="text"
+              class="form-control form-control-lg"
+              placeholder="Username"
+            />
+          </fieldset>
+          <fieldset class="form-group">
+            <input
+              type="text"
+              class="form-control form-control-lg"
+              placeholder="Email"
+            />
+          </fieldset>
+          <fieldset class="form-group">
+            <input
+              type="password"
+              class="form-control form-control-lg"
+              placeholder="Password"
+            />
+          </fieldset>
+          <button type="submit" class="btn btn-lg btn-primary pull-xs-right">
+            Sign Up
+          </button>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
-<script>
-</script>
+<script></script>
