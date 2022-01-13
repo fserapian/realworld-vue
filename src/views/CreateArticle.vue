e3807f18eb921f3b6e866fab6cda7feb6666a5f3
Fadi Serapian <fadi.serapian@gmail.com>
2022-01-13 18:36:56 -0500

    add CreateArticle vue with child ArticleForm

---
+++ src/views/CreateArticle.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>Create Article</h2>
+    <app-article-form></app-article-form>
+  </div>
+</template>
+
+<script>
+import AppArticleForm from '@/components/ArticleForm';
+
+export default {
+  name: 'AppCreateArticle',
+  components: {
+    AppArticleForm,
+  },
+};
+</script>
